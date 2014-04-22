3ed43249adc2816ce821110ba2ab381ca6739e91
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-04-22 13:50:31 +0200

    [test/sqlserver] fix imports

--- test/unittest_sqlserver2005.py
+++ test/unittest_sqlserver2005.py
@@ -15,11 +15,10 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with logilab-database. If not, see <http://www.gnu.org/licenses/>.
+import sys
 import unittest
 
 from logilab.common.testlib import TestCase, MockConnection
-
-from unittest_fti import IndexableObject
 
 from logilab.database import get_db_helper
 
