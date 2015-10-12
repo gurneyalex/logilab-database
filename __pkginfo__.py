4a12685225a6af2d52225fd6ed5063507d0adf80
R?mi Cardona <remi.cardona@logilab.fr>
2015-10-12 14:35:16 +0200

    [pkg] Use setuptools
    * inside setup.py
    * update repository layout
    * update debian and centos/rhel packaging accordingly
    
    Closes #2083027.

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -17,6 +17,8 @@
 # with logilab-database. If not, see <http://www.gnu.org/licenses/>.
 """logilab.database packaging information."""
 
+import sys
+
 distname = 'logilab-database'
 modname = 'database'
 numversion = (1, 13, 4)
@@ -29,13 +31,26 @@ author_email = "contact@logilab.fr"
 description = "true unified database access"
 
 web = "http://www.logilab.org/project/%s" % distname
-ftp = "ftp://ftp.logilab.org/pub/%s" % modname
 mailinglist = "mailto://python-projects@lists.logilab.org"
 
 subpackage_of = 'logilab'
-subpackage_master = True
 
 install_requires = [
+    'setuptools',
     'logilab-common >= 0.63.2',
     'six >= 1.4.0',
+    'Yapps2',
     ]
+
+tests_require = [
+    'psycopg2',
+    ]
+
+if sys.version_info[0] == 2:
+    tests_require.append('MySQL-python')
+
+classifiers = ["Topic :: Database",
+               "Programming Language :: Python",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 3",
+               ]
