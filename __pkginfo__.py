1c7d15ce27f4970617f2f685d46bea1bebb19761
Denis Laxalde <denis.laxalde@logilab.fr>
2019-05-17 17:24:11 +0200

    [pkg] Version 1.17.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -21,7 +21,7 @@ import sys
 
 distname = 'logilab-database'
 modname = 'database'
-numversion = (1, 16, 0)
+numversion = (1, 17, 0)
 version = '.'.join([str(num) for num in numversion])
 license = 'LGPL'
 
